54f7e6144603d6e9593ff3b92c0e8b4a7f27af05
Nikita Savelev <nsavelev@yahoo.com>
2020-03-10 14:05:10 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,7 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
+<<<<<<< HEAD
  // return Object.assign({}, playlist, { artistName: songTitle })
  playlist[artistName] = songTitle
 }
@@ -12,3 +13,12 @@ function removeFromPlaylist(playlist, artistName){
  delete playlist[artistName]
   
 }
+=======
+  return Object.assign({},{ artistName: songTitle })
+}
+
+//function removeFromPlaylist(playlist, artistName){
+ //delete playlist.artistName
+  
+//}
+>>>>>>> cc59c184288d2d8a612c466fe5d35faeb3375aa2
